89e310ad509b8f40cb57596a964f7aee345dfdf9
Juan <jmponceguajardo@gmail.com>
2023-10-30 18:24:10 -0300

    se agrega link a proyectos

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,8 @@
-import { Box, Container, Grid, Typography } from "@mui/material";
+/* eslint-disable react/jsx-no-target-blank */
+import { Box, Container, Grid, Tooltip, Typography } from "@mui/material";
 import PlusButton from "../../components/plusButton/plusButton";
 import "./Home.css";
+import ContactButtonGroup from "../../components/contactButtonGroup/ContactButtonGroup";
 const Home = () => {
   return (
     <Container maxWidth="fluid" id="homeApp">
@@ -15,7 +17,9 @@ const Home = () => {
               Un programador frontend en crecimiento. Me gusta crear, diseñar y
               aprender cosas nuevas.
             </Typography>
-            <PlusButton />
+            <Tooltip title="Mas sobre mí" disableFocusListener>
+              <PlusButton />
+            </Tooltip>
           </Grid>
           <Grid lg={3} sm={3} item bgcolor={"#3333"} className="grid1y2">
             <img src="../public/juan.png" alt="Juan" className="profileImage" />
@@ -29,11 +33,13 @@ const Home = () => {
         </Typography>
         <Grid container spacing={0}>
           <Grid lg={4} sm={4} item bgcolor={"#4333"}>
-            <img
-              src="../public/icono.png"
-              alt="arabfolc"
-              className="proyimagen"
-            />
+            <a href="https://juanarabfolk.com/" target="_blank">
+              <img
+                src="../public/icono.png"
+                alt="arabfolc"
+                className="proyimagen"
+              />
+            </a>
           </Grid>
           <Grid lg={4} sm={4} item bgcolor={"#4333"}>
             <img src="../public/robyx.png" alt="robyx" className="proyimagen" />
@@ -49,9 +55,12 @@ const Home = () => {
       </Box>
 
       <Box className="contacto" id="contacto">
-        <Typography variant="h4" className="titlePage">
+        <Typography variant="h2" className="titlePage">
           Contacto
         </Typography>
+        <Box>
+          <ContactButtonGroup />
+        </Box>
       </Box>
     </Container>
   );
