7b03beeb24fe668ccf54cee978f9a0fad9f7281f
Juan <jmponceguajardo@gmail.com>
2023-10-31 18:51:58 -0300

    se estila directamente en jsx

--- src/components/contactButtonGroup/ContactButtonGroup.jsx
+++ src/components/contactButtonGroup/ContactButtonGroup.jsx
@@ -1,23 +1,45 @@
+import "./ContactButtonGroup.css";
 import { Button, ButtonGroup } from "@mui/material";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import EmailIcon from "@mui/icons-material/Email";
 import { green, indigo, red } from "@mui/material/colors";
-import "./ContactButtonGroup.css";
+
 const ContactButtonGroup = () => {
   return (
-    <ButtonGroup variant="outlined" size="large">
-      <Button className="linkedin">
+    <ButtonGroup variant="outlined" size="large" color="primary">
+      <Button
+        className="linkedin"
+        sx={{
+          ":hover": {
+            backgroundColor: indigo["100"],
+          },
+        }}
+      >
         <a href="http://" target="_blank" rel="noopener noreferrer">
           <LinkedInIcon fontSize="large" sx={{ color: indigo["900"] }} />
         </a>
       </Button>
-      <Button className="whatsapp">
+      <Button
+        className="whatsapp"
+        sx={{
+          ":hover": {
+            backgroundColor: green["100"],
+          },
+        }}
+      >
         <a href="http://" target="_blank" rel="noopener noreferrer">
           <WhatsAppIcon fontSize="large" sx={{ color: green["A700"] }} />
         </a>
       </Button>
-      <Button className="gmail">
+      <Button
+        className="gmail"
+        sx={{
+          ":hover": {
+            backgroundColor: red["100"],
+          },
+        }}
+      >
         <a href="http://" target="_blank" rel="noopener noreferrer">
           <EmailIcon fontSize="large" sx={{ color: red["600"] }} />
         </a>
