f1eec05742916e2038cfc226a3afba4a3541ddd3
Juan <jmponceguajardo@gmail.com>
2023-11-02 17:29:53 -0300

    adaptando para celular y movil

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,43 +1,49 @@
-/* eslint-disable react/jsx-no-target-blank */
-import { Box, Container, Grid, Tooltip, Typography } from "@mui/material";
+import { Container, Box, Grid, Typography } from "@mui/material";
 import PlusButton from "../../components/plusButton/plusButton";
 import "./Home.css";
 import ContactButtonGroup from "../../components/contactButtonGroup/ContactButtonGroup";
 import KnowledgeButtonGroup from "../../components/knowledgeButtonGroup/KnowledgeButtonGroup";
+
 const Home = () => {
   return (
     <Container maxWidth="fluid" id="homeApp">
+      {/* Secciones de Sobre Mí */}
       <Box className="sobreMi" id="sobreMi">
-        <Typography variant="h1" className="titlePage">
+        <Typography variant="h2" className="titlePage">
           Hola! Soy Juanma!
         </Typography>
-
-        <Grid container spacing={0}>
-          <Grid lg={9} sm={3} item bgcolor={"#333"} className="grid1y2">
-            <Typography variant="h4" className="sobremiDescripcion">
+        <Box className="contenedorSobreMi">
+          <Box bgcolor={"#10110c"} className="divDescripcion">
+            <Typography variant="h3" className="sobremiDescripcion">
               Un programador frontend en crecimiento. Me gusta crear, diseñar y
               aprender cosas nuevas.
             </Typography>
-            <Tooltip title="Mas sobre mí" disableFocusListener>
-              <PlusButton />
-            </Tooltip>
-          </Grid>
-          <Grid lg={3} sm={3} item bgcolor={"#3333"} className="grid1y2">
-            <img src="../public/juan.png" alt="Juan" className="profileImage" />
-          </Grid>
-          <Box>
-            <KnowledgeButtonGroup />
+            <Typography variant="h6" className="sobremiDescripcion2">
+              Más sobre mí:
+            </Typography>
+            <PlusButton />
           </Box>
-        </Grid>
+          <Box bgcolor={"#10110c"} className="sobreMiFotoDiv">
+            <img src="../public/juan.png" alt="Juan" className="fotoPerfil" />
+            <Box className="conocimientos">
+              <KnowledgeButtonGroup />
+            </Box>
+          </Box>
+        </Box>
       </Box>
 
+      {/* Sección de Proyectos */}
       <Box className="proyectos" id="proyectos">
         <Typography variant="h2" className="titlePage">
           Proyectos
         </Typography>
-        <Grid container spacing={0}>
-          <Grid lg={4} sm={4} item bgcolor={"#4333"}>
-            <a href="https://juanarabfolk.com/" target="_blank">
+        <Grid container spacing={2}>
+          <Grid lg={4} sm={6} item>
+            <a
+              href="https://juanarabfolk.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <img
                 src="../public/icono.png"
                 alt="arabfolc"
@@ -45,10 +51,10 @@ const Home = () => {
               />
             </a>
           </Grid>
-          <Grid lg={4} sm={4} item bgcolor={"#4333"}>
+          <Grid lg={4} sm={6} item>
             <img src="../public/robyx.png" alt="robyx" className="proyimagen" />
           </Grid>
-          <Grid lg={4} sm={4} item bgcolor={"#4333"}>
+          <Grid lg={4} sm={6} item>
             <img
               src="../public/encriptado.png"
               alt="encriptado"
@@ -58,11 +64,12 @@ const Home = () => {
         </Grid>
       </Box>
 
+      {/* Sección de Contacto */}
       <Box className="contacto" id="contacto">
         <Typography variant="h2" className="titlePage">
           Contacto
         </Typography>
-        <Box marginLeft={"50rem"}>
+        <Box className="divGrupoDeBotones">
           <ContactButtonGroup />
         </Box>
       </Box>
